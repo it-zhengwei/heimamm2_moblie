c16c55f23562f63a8f4b548a7801772f0f6f0af8
肌肉仔 <itzhengwei163.com>
2020-07-24 18:25:07 +0800

    下载和导入flexible  根据设备尺寸改变rem大小值

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 导入flexible 根据设备的尺寸改变rem大小
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
 new Vue({
