711521707ab7028d7f828a1ef6e0205e50f0e1dd
肌肉仔 <itzhengwei163.com>
2020-07-24 19:17:46 +0800

    导入初始化样式

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,10 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="less">
+div {
+  width: 100px;
+  height: 100px;
+  background-color: @assist-color;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入初始化样式
+import '@/style/base.less'
 // 导入normalize.css  抹平差异样式  让页面有一个统一的样式
 import 'normalize.css'
 // 导入flexible 根据设备的尺寸改变rem大小
