287198ccfc634c675cc188933ce4f0314ae8452a
肌肉仔 <itzhengwei163.com>
2020-07-26 17:55:46 +0800

    导航的tabbar完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <template>
-  <div>
-    哈哈
+  <div class="app">
+    <router-view></router-view>
+    <van-tabbar
+      v-model="active"
+      route
+      active-color="#e40137"
+      inactive-color="#B4B4BD"
+    >
+      <van-tabbar-item to="/company">
+        <span>公司</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_gs_nor"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/question">
+        <span>刷题</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_st_nor"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/find">
+        <span>发现</span>
+        <template #icon>
+          <i class="iconfont iconicon_liulanliang"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/my">
+        <span>我的</span>
+        <template #icon>
+          <i class="iconfont iconicon_footbar_wode_sel"></i>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.app {
+  i {
+    font-size: 28px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+// 导入组件
+// 公司组件
+import company from '@/views/company/index.vue'
+// 发现组件
+import find from '@/views/find/index.vue'
+// 我的组件
+import my from '@/views/my/index.vue'
+// 问题组件
+import question from '@/views/question/index.vue'
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    redirect: '/company'
+  },
+  {
+    path: '/company',
+    component: company
+  },
+  {
+    path: '/find',
+    component: find
+  },
+  {
+    path: '/my',
+    component: my
+  },
+  {
+    path: '/question',
+    component: question
+  }
+]
 
 const router = new VueRouter({
   routes
