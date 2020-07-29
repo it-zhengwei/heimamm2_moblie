0ec5dae10c4a1a61a10765450c1b4ccf0675f16b
肌肉仔 <itzhengwei163.com>
2020-07-29 19:53:37 +0800

    处理请求异常

--- src/utils/copy.js
+++ src/utils/copy.js
@@ -14,10 +14,14 @@ copy.interceptors.request.use(
 // 响应拦截器
 copy.interceptors.response.use(
   res => {
-    if (res.data.code === 200) {
+    // 声明一个常量保存code
+    const code = res.data.code
+    if (code === 200) {
       return res.data
+    } else if (code === 400 || code === 401 || code === 403) {
+      return Promise.reject(new Error(res.data.message))
     } else {
-      return Promise.reject(res.data.message)
+      return Promise.reject(new Error())
     }
   },
   err => {
